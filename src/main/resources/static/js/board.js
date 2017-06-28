7a47d4b43a87f38ee011ac03fec5ef5f1493c4a3
Pahnki Kim <pahnki.kim@pkworks.io>
2017-06-28 10:32:01 +0900

    apply changeSection with state

--- src/main/resources/static/js/board.js
+++ src/main/resources/static/js/board.js
@@ -2,7 +2,15 @@ $(function() {
 	function changeSection(section) {
 		$('.board-section').hide();
 		$('.board-' + section).show();
+		
+		history.pushState({}, null, '#' + section);
 	}
+	
+	$(window).on('popstate', function() {
+		var section = location.hash.replace('#', '');
+		
+		changeSection(section);
+	});
 	
 	$('.board-link').on('click', function() {
 		var link = $(this).attr('link');
