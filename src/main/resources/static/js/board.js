ac419f6c9586398ca00a40ef676ad52ca6a86c37
Pahnki Kim <pahnki.kim@pkworks.io>
2017-06-28 15:56:48 +0900

    update pagination ui

--- src/main/resources/static/js/board.js
+++ src/main/resources/static/js/board.js
@@ -1,4 +1,8 @@
 $(function() {
+	var currentPage = 1;
+	var rowsPerPage = 5;
+	var pagesToShow = 2;
+	
 	function changeSection(section, init) {
 		$('.board-section').hide();
 		$('.board-' + section).show();
@@ -45,6 +49,75 @@ $(function() {
 			
 			$('#board-list > tbody').append(rowHtml);
 		}
+		
+		requestPaging();
+	}
+	
+	function requestPaging() {
+		$.ajax({
+			url: '/total',
+			success: function(result) {
+				var total = result.total;
+				
+				setPaging(total);
+			}
+		});
+	}
+	
+	function setPaging(total) {
+		var rowsPerPage = 5;
+		var totalPages = total / rowsPerPage
+			+ (total % rowsPerPage === 0 ? 0 : 1);
+		var firstPage = 1;
+		var lastPage = totalPages;
+		var startPage = ((currentPage - 1) / pagesToShow)
+			* pagesToShow + 1;
+		var endPage = Math.min(startPage + pagesToShow - 1, lastPage);
+		var prevPage = startPage - 1;
+		var nextPage = endPage + 1;
+		
+		$('.board-list .pagination').empty();
+		
+		var pagingHtml = '';
+		pagingHtml += '<li>';
+		pagingHtml += '<a class="board-page"';
+		pagingHtml += ' page="' + prevPage + '"';
+		pagingHtml += ' href="#" aria-label="Previous">';
+		pagingHtml += '<span aria-hidden="true">&laquo;</span>';
+		pagingHtml += '</a>';			
+		pagingHtml += '</li>';
+		
+		for (var i=startPage; i<=endPage; i++) {
+			pagingHtml += '<li';
+			
+			if (i === currentPage) {
+				pagingHtml += ' class="active"';
+			}
+			
+			pagingHtml += '><a class="board-page"';
+			pagingHtml += ' page="' + i + '"';
+			pagingHtml += ' href="#">' + i + '</a></li>';
+		}
+		
+		pagingHtml += '<li>';
+		pagingHtml += '<a class="board-page"';
+		pagingHtml += ' page="' + nextPage + '"';
+		pagingHtml += ' href="#" aria-label="Next">';
+		pagingHtml += '<span aria-hidden="true">&raquo;</span>';
+		pagingHtml += '</a>';
+		pagingHtml += '</li>';
+		
+		$('.board-list .pagination').html(pagingHtml);
+		
+		handlePagingEvent();
+	}
+	
+	function handlePagingEvent() {
+		$('.board-page').on('click', function(event) {
+			event.preventDefault();
+			
+			var page = $(this).attr('page');
+		});
 	}
 	
 	$(window).on('popstate', function() {
